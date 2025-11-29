d0d37df920759bffec13e5bd3210920ac9562ad3
gasser <gaser.khaled25@gmail.com>
2025-11-29 02:28:07 +0200

    Update API base URL to use environment variable for better configuration management

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 ﻿import axios from 'axios';
 import auth from '../api/auth.js';
 
-const API_BASE_URL = 'https://gasserkhaled-001-site1.anytempurl.com/api';
-const API_BASE_URL_Development = 'https://localhost:7151/api';
+const API_BASE_URL =
+	import.meta.env.VITE_API_BASE_URL || 'https://localhost:7151/api';
 
 const axiosInstance = axios.create({
 	baseURL: API_BASE_URL,
